2592dada4e8fb34a7b9b007add76744d25ad3d2e
Muhammad Bilal Qamar <42898923+MuhammadBilalQamar@users.noreply.github.com>
2019-08-13 10:11:34 +0500

    Update Firebase.js

--- config/Firebase.js
+++ config/Firebase.js
@@ -1,13 +1,7 @@
 import Firebase from 'firebase';
 let config = {
   // enter your firebase script here
-apiKey: "AIzaSyDxhWj6kuxqxdP_yvV6Is5AWeKJx9abzIw",
-  authDomain: "fir-7a8a8.firebaseapp.com",
-  databaseURL: "https://fir-7a8a8.firebaseio.com",
-  projectId: "fir-7a8a8",
-  storageBucket: "fir-7a8a8.appspot.com",
-  messagingSenderId: "671690736383",
-  appId: "1:671690736383:web:9feed1032008ce77"
+
 };
 let app = Firebase.initializeApp(config);
 export const db = app.database();
